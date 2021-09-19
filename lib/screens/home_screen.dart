9d80dcbdbb187124f3e7cd0872f413820c38e526
Germano-Dev <88905779+Germano-Dev@users.noreply.github.com>
2021-09-19 16:09:18 -0300

    commit
    continuação da criação dos layouts.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:english/utils/gradient.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -10,11 +11,129 @@ class HomeScreen extends StatelessWidget {
         title: Text('English Evolution'),
         backgroundColor: Colors.red,
       ),
-      body: Container(
-        child: Stack(
+      body: Scaffold(
+        body: Stack(
           children: [
-            //GradientTheme(),
-            Center(child: Text('Teste de página')),
+            GradientTheme(),
+            Positioned(
+              top: 30,
+              left: 65,
+              right: 65,
+              child: Text(
+                'Selecione um Nível',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Positioned(
+              top: 100,
+              left: 50,
+              right: 50,
+              child: Text(
+                'Escolha seu nível de aprendizado e, \n começe a aprender Inglês de forma \n divertida e dimânica.',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.red,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Positioned(
+              top: 230,
+              left: 80,
+              right: 80,
+              child: Column(
+                children: [
+                  SizedBox(
+                    width: 250,
+                    height: 50,
+                    child: ElevatedButton(
+                      child: Text(
+                        'Básico',
+                      ),
+                      onPressed: () {},
+                      style: ButtonStyle(
+                        foregroundColor:
+                            MaterialStateProperty.all<Color>(Colors.white),
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                            Colors.blue.withOpacity(1.0)),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16.0),
+                            side: BorderSide(color: Colors.red),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 50),
+                  SizedBox(
+                    width: 250,
+                    height: 50,
+                    child: ElevatedButton(
+                      child: Text('Intermediário'),
+                      onPressed: () {},
+                      style: ButtonStyle(
+                        foregroundColor:
+                            MaterialStateProperty.all<Color>(Colors.blue),
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                            Colors.white.withOpacity(1.0)),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16.0),
+                            side: BorderSide(color: Colors.red),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 50),
+                  SizedBox(
+                    width: 250,
+                    height: 50,
+                    child: ElevatedButton(
+                      child: Text(
+                        'Avançado',
+                      ),
+                      onPressed: () {},
+                      style: ButtonStyle(
+                        foregroundColor:
+                            MaterialStateProperty.all<Color>(Colors.white),
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                            Colors.red.withOpacity(1.0)),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16.0),
+                            side: BorderSide(color: Colors.blue),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 250,
+                    height: 200,
+                    margin: EdgeInsets.only(top: 50),
+                    padding: EdgeInsets.only(
+                        top: 05, bottom: 05, right: 30, left: 30),
+                    decoration: BoxDecoration(
+                      color: Colors.white30,
+                      borderRadius: BorderRadius.circular(16),
+                      border: Border.all(color: Colors.blue),
+                    ),
+                    child: SingleChildScrollView(
+                      child: Text(
+                          'Aqui aparecerá uma breve descrição de cada nível!'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
